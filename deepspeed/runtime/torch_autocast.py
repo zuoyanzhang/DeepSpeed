Fix scaling and allgather with `torch.autocast` (#7534)
This PR includes these two fixes:
- Use GradScaler only for FP16 (not for BF16)
- Fix dtype conversion for ZeRO3 allgather
- The reduce hook should be called only once, even when a parameter is
shared across multiple layers (tied parameters).
- Currently, the hook is triggered at each tied layer because we
temporarily set `.data` with a different dtype.
- The fix ensures that the parameter consistently retains the same
dtype.

---------

Signed-off-by: Masahiro Tanaka <mtanaka@anyscale.com>
Signed-off-by: Olatunji Ruwase <tunji.ruwase@snowflake.com>
Signed-off-by: Stas Bekman <stas@stason.org>
Signed-off-by: jakehemmerle <jakehemmerle@protonmail.com>
Signed-off-by: Qi Bin <qibin0506@users.noreply.github.com>
Co-authored-by: Olatunji Ruwase <tunji.ruwase@snowflake.com>
Co-authored-by: Stas Bekman <stas00@users.noreply.github.com>
Co-authored-by: digger yu <digger-yu@outlook.com>
Co-authored-by: Jake Hemmerle <jakehemmerle@gmail.com>
Co-authored-by: Logan Adams <114770087+loadams@users.noreply.github.com>
Co-authored-by: Qi Bin <qibin0506@users.noreply.github.com>

deepspeed/runtime/torch_autocast.py
@@ -16,8 +16,12 @@ LOWER_PRECISION_SAFE_MODULES = [
     torch.nn.Conv3d,
 ]
 
-TORCH_AUTOCAST_INITIALIZED = False
+PARAM_COMM_DTYPE_ATTR_NAME = "comm_dtype"
 _WARNED_NESTED_AUTOCAST = False
+
+# TODO: Avoid using global variables
+TORCH_AUTOCAST_INITIALIZED = False
+TORCH_AUTOCAST_DTYPE = None
 
 
 def _validate_auto_cast_settings(engine):
@@ -53,10 +57,12 @@ def init_autocast_params(engine, dtype: torch.dtype,
     for module in model.modules():
         if module.__class__ in lower_precision_safe_module_classes:
             for p in module.parameters(recurse=False):
-                p.autocast_dtype = dtype
+                setattr(p, PARAM_COMM_DTYPE_ATTR_NAME, dtype)
 
     global TORCH_AUTOCAST_INITIALIZED
     TORCH_AUTOCAST_INITIALIZED = True
+    global TORCH_AUTOCAST_DTYPE
+    TORCH_AUTOCAST_DTYPE = dtype
 
 
 def is_autocast_initialized() -> bool:
@@ -67,16 +73,20 @@ def get_default_autocast_lower_precision_modules() -> List[str]:
     return [f"{cls.__module__}.{cls.__name__}" for cls in LOWER_PRECISION_SAFE_MODULES]
 
 
-def get_autocast_dtype(param: torch.nn.Parameter) -> torch.dtype:
-    return param.autocast_dtype if hasattr(param, "autocast_dtype") else param.dtype
+def get_autocast_dtype() -> torch.dtype:
+    return TORCH_AUTOCAST_DTYPE
 
 
-def has_autocast_dtype(param: torch.nn.Parameter) -> bool:
-    return hasattr(param, "autocast_dtype")
+def has_comm_dtype(param: torch.nn.Parameter) -> bool:
+    return hasattr(param, PARAM_COMM_DTYPE_ATTR_NAME)
 
 
-def get_all_autocast_dtypes(params: Iterable) -> Set[torch.dtype]:
-    return {get_autocast_dtype(p) for p in params}
+def get_comm_dtype(param: torch.nn.Parameter) -> torch.dtype:
+    return getattr(param, PARAM_COMM_DTYPE_ATTR_NAME, param.dtype)
+
+
+def get_all_comm_dtypes(params: Iterable) -> Set[torch.dtype]:
+    return {get_comm_dtype(p) for p in params}
 
 
 def sort_dtypes(dtypes: List[torch.dtype]) -> List[torch.dtype]:

deepspeed/runtime/zero/stage3.py
@@ -19,7 +19,7 @@ from deepspeed.runtime.base_optimizer import ZeROOptimizer
 from deepspeed.utils import logger
 from deepspeed.utils.torch import register_grad_hook
 from deepspeed.runtime.fp16.loss_scaler import CreateLossScaler
-from deepspeed.runtime.torch_autocast import get_all_autocast_dtypes, is_autocast_initialized, sort_dtypes
+from deepspeed.runtime.torch_autocast import get_autocast_dtype, get_all_comm_dtypes, is_autocast_initialized, sort_dtypes
 from deepspeed.runtime.comm.coalesced_collectives import reduce_scatter_coalesced, all_to_all_quant_reduce, all_to_all_loco_quant_reduce
 from deepspeed.runtime.utils import inf, is_model_parallel_parameter, get_only_unique_item, mask_nan_or_inf_with_val_inplace
 from deepspeed.runtime.zero.partition_parameters import *
@@ -394,12 +394,13 @@ class DeepSpeedZeroOptimizer_Stage3(ZeROOptimizer):
         # map between param_id and bool to specify if a param is in this partition
         self.is_param_in_current_partition = {}
 
+        self.torch_autocast_gradscaler = None
         if is_autocast_initialized():
-            comm_dtypes = get_all_autocast_dtypes([p for params in self.fp16_groups for p in params])
-            self.torch_autocast_gradscaler = torch.amp.GradScaler(device=get_accelerator().device_name())
+            comm_dtypes = get_all_comm_dtypes([p for params in self.fp16_groups for p in params])
+            if get_autocast_dtype() == torch.float16:
+                self.torch_autocast_gradscaler = torch.amp.GradScaler(device=get_accelerator().device_name())
         else:
             comm_dtypes = {self.communication_data_type}
-            self.torch_autocast_gradscaler = None
 
         self.ipg_buckets: Dict[torch.dtype, IPGBucketZ3] = {dtype: IPGBucketZ3() for dtype in comm_dtypes}
 
@@ -987,19 +988,27 @@ class DeepSpeedZeroOptimizer_Stage3(ZeROOptimizer):
     def _optimizer_step(self, sub_group_id):
         param_group_id = self.sub_group_to_group_id[sub_group_id]
         fp32_param = self.fp32_partitioned_groups_flat[sub_group_id]
+
+        def step_with_gradscaler(optimizer):
+            if self.torch_autocast_gradscaler:
+                self.torch_autocast_gradscaler.step(optimizer)
+                self.torch_autocast_gradscaler.update()
+            else:
+                optimizer.step()
+
         if self.offload_optimizer:
             cur_device = self.subgroup_to_device[sub_group_id]
             if cur_device == 'cpu':
                 self.optimizer.param_groups[param_group_id]['params'] = [fp32_param]
-                cpu_loss = self.optimizer.step()
+                step_with_gradscaler(self.optimizer)
                 self.optimizer.param_groups[param_group_id]['params'] = []
             else:
                 self.backup_optimizer.param_groups[param_group_id]['params'] = [fp32_param]
-                gpu_loss = self.backup_optimizer.step()
+                step_with_gradscaler(self.backup_optimizer)
                 self.backup_optimizer.param_groups[param_group_id]['params'] = []
         else:
             self.optimizer.param_groups[param_group_id]['params'] = [fp32_param]
-            self.optimizer.step()
+            step_with_gradscaler(self.optimizer)
             self.optimizer.param_groups[param_group_id]['params'] = []
 
     def _swappable_optimizer_subgroup(self, sub_group_id):
@@ -2303,6 +2312,8 @@ class DeepSpeedZeroOptimizer_Stage3(ZeROOptimizer):
         if self.custom_loss_scaler:
             scaled_loss = self.external_loss_scale * loss
             scaled_loss.backward()
+        elif self.torch_autocast_gradscaler:
+            self.torch_autocast_gradscaler.scale(loss).backward(retain_graph=retain_graph)
         else:
             self.loss_scaler.backward(loss.float(), retain_graph=retain_graph)
 

deepspeed/runtime/zero/stage_1_and_2.py
@@ -18,7 +18,7 @@ from typing import Container
 from deepspeed.runtime.zero.offload_states import offload_optimizer_states, reload_optimizer_states
 from deepspeed.runtime.base_optimizer import ZeROOptimizer
 from deepspeed.runtime.fp16.loss_scaler import CreateLossScaler
-from deepspeed.runtime.torch_autocast import get_all_autocast_dtypes, is_autocast_initialized, sort_dtypes
+from deepspeed.runtime.torch_autocast import get_autocast_dtype, get_all_comm_dtypes, is_autocast_initialized, sort_dtypes
 from deepspeed.runtime.utils import (empty_cache, see_memory_usage, inf, is_model_parallel_parameter,
                                      align_dense_tensors, all_gather_dp_groups, mask_nan_or_inf_with_val_inplace)
 from deepspeed.runtime.zero.config import ZeroStageEnum
@@ -484,12 +484,13 @@ class DeepSpeedZeroOptimizer(ZeROOptimizer):
         # map between param_id and bool to specify if a param is in this partition
         self.is_param_in_current_partition = {}
 
+        self.torch_autocast_gradscaler = None
         if is_autocast_initialized():
-            comm_dtypes = get_all_autocast_dtypes([p for params in self.bit16_groups for p in params])
-            self.torch_autocast_gradscaler = torch.amp.GradScaler(device=get_accelerator().device_name())
+            comm_dtypes = get_all_comm_dtypes([p for params in self.bit16_groups for p in params])
+            if get_autocast_dtype() == torch.float16:
+                self.torch_autocast_gradscaler = torch.amp.GradScaler(device=get_accelerator().device_name())
         else:
             comm_dtypes = {self.communication_data_type}
-            self.torch_autocast_gradscaler = None
 
         self.ipg_buckets: Dict[torch.dtype, IPGBucket] = {dtype: IPGBucket() for dtype in comm_dtypes}
 